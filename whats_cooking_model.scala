332640b3166e24145886822b9355fc618586b7d0
Robert Dodier <robert_dodier@users.sourceforge.net>
2015-11-15 13:02:26 -0800

    Fix bug in false negative calculation.

--- whats_cooking_model.scala
+++ whats_cooking_model.scala
@@ -8,7 +8,7 @@ model.getThreshold
 val foo = x.map (p => (model.predict (p.features), p.label))
 val n11 = foo.filter (ab => ab._1 == 1.0 && ab._2 == 1.0).count
 val n10 = foo.filter (ab => ab._1 == 1.0 && ab._2 == 0.0).count
-val n01 = foo.filter (ab => ab._1 == 0.0 && ab._2 == 0.1).count
+val n01 = foo.filter (ab => ab._1 == 0.0 && ab._2 == 1.0).count
 val n00 = foo.filter (ab => ab._1 == 0.0 && ab._2 == 0.0).count
 model.clearThreshold
 val bar = x.map (p => (p.label, model.predict (p.features)))
