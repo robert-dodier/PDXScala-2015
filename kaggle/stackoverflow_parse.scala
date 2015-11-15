1f323295c3d2d676fce27c74d2ca080fe02f0001
Robert Dodier <robert_dodier@users.sourceforge.net>
2015-11-14 16:36:11 -0800

    Correct some bugs and change all Integer to Int.

--- kaggle/stackoverflow_parse.scala
+++ kaggle/stackoverflow_parse.scala
@@ -1,27 +1,28 @@
 import org.apache.spark.rdd.RDD
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
+import java.util.Date
 
 case class stackoverflow_record_transformed
  (OwnerAgeAtPostCreationInDays : Double,
   OwnerUserId : Long,
-  ReputationAtPostCreation : Integer,
-  OwnerUndeletedAnswerCountAtPostTime : Integer,
-  OwnerClosedPostCountAtPostTime : Integer,
+  ReputationAtPostCreation : Int,
+  OwnerUndeletedAnswerCountAtPostTime : Int,
+  OwnerClosedPostCountAtPostTime : Int,
   Tag1 : String,
   Tag2 : String,
   Tag3 : String,
   Tag4 : String,
   Tag5 : String,
-  StatusClosedForAnyReason : Integer)
+  StatusClosedForAnyReason : Int)
   
 case class stackoverflow_record
  (PostId : Long,
   PostCreationDate : java.util.Date,
   OwnerUserId : Long,
   OwnerCreationDate : java.util.Date,
-  ReputationAtPostCreation : Integer,
-  OwnerUndeletedAnswerCountAtPostTime : Integer,
+  ReputationAtPostCreation : Int,
+  OwnerUndeletedAnswerCountAtPostTime : Int,
   Title : String,
   BodyMarkdown : String,
   Tag1 : String,
@@ -34,24 +35,32 @@ case class stackoverflow_record
 
 object stackoverflow_parse
 {
+  def read_stackoverflow_records_transformed (sc : SparkContext, filename : String) =
+  {
+    val x = read_stackoverflow_records (sc, filename)
+    val x1 = count_closed_posts (x)
+    transform_stackoverflow_records (x1)
+  }
+
   // Return tuple comprising:
   // OwnerAgeAtPostCreationInDays : Double,
-  // ReputationAtPostCreation : Integer,
-  // OwnerUndeletedAnswerCountAtPostTime : Integer,
-  // OwnerClosedPostCountAtPostTime : Integer,
+  // ReputationAtPostCreation : Int,
+  // OwnerUndeletedAnswerCountAtPostTime : Int,
+  // OwnerClosedPostCountAtPostTime : Int,
   // Tag1 : String,
   // Tag2 : String,
   // Tag3 : String,
   // Tag4 : String,
   // Tag5 : String,
-  // StatusClosedForAnyReason : Integer
-//def transform_stackoverflow_records (x : RDD [(Long, (Tuple13 [java.util.Date, Long, java.util.Date, Integer, Integer, String, String, String, String, String, String, String, Option [java.util.Date], String], Integer))]) =
-//{
-//  x.map { case (OwnerUserId, ((PostCreationDate, OwnerCreationDate, ReputationAtPostCreation, OwnerUndeletedAnswerCountAtPostTime, Title, BodyMarkdown, Tag1, Tag2, Tag3, Tag4, Tag5, PostClosedDate, OpenStatus), OwnerClosedPostCountAtPostTime)) => 
-//    val OwnerAgeAtPostCreationInDays = (PostCreationDate.getTime - OwnerCreationDate.getTime) / (86400.0 * 1000.0)
-//    val StatusClosedForAnyReason = OpenStatus != "open"
-//    (OwnerAgeAtPostCreationInDays, ReputationAtPostCreation, OwnerUndeletedAnswerCountAtPostTime, OwnerClosedPostCountAtPostTime, Tag1, Tag2, Tag3, Tag4, Tag5, StatusClosedForAnyReason) }
-//}
+  // StatusClosedForAnyReason : Int
+//def transform_stackoverflow_records (x : RDD [(Long, (Tuple13 [java.util.Date, Long, java.util.Date, Int, Int, String, String, String, String, String, String, String, Option [java.util.Date], String], Int))]) =
+  def transform_stackoverflow_records (x : RDD [(Long, ((java.util.Date, java.util.Date, Int, Int, String, String, String, String, String, String, String, Option [java.util.Date], String), Int))]) =
+  {
+    x.map { case (ownerUserId : Long, ((postCreationDate : java.util.Date, ownerCreationDate : java.util.Date, reputationAtPostCreation : Int, ownerUndeletedAnswerCountAtPostTime : Int, title : String, bodyMarkdown : String, tag1 : String, tag2 : String, tag3 : String, tag4 : String, tag5 : String, postClosedDate : Option [Date], openStatus : String), ownerClosedPostCountAtPostTime : Int)) => 
+      val ownerAgeAtPostCreationInDays = (postCreationDate.getTime - ownerCreationDate.getTime) / (86400.0 * 1000.0)
+      val statusClosedForAnyReason = openStatus != "open"
+      (ownerAgeAtPostCreationInDays, reputationAtPostCreation, ownerUndeletedAnswerCountAtPostTime, ownerClosedPostCountAtPostTime, tag1, tag2, tag3, tag4, tag5, statusClosedForAnyReason) }
+  }
 
   def read_stackoverflow_records (sc : SparkContext, filename : String) =
   {
@@ -78,9 +87,9 @@ object stackoverflow_parse
     parsed
   }
 
-  def count_closed_posts (x : RDD [(Long, Tuple14 [java.util.Date, Long, java.util.Date, Integer, Integer, String, String, String, String, String, String, String, Option [java.util.Date], String])]) =
+  def count_closed_posts (x : RDD [(Long, (java.util.Date, java.util.Date, Int, Int, String, String, String, String, String, String, String, Option [java.util.Date], String))]) =
   {
-      val x1 = x.aggregateByKey(0)((acc, value) => acc + (if (value._14 == "open") 0 else 1), (acc1, acc2) => acc1 + acc2)
+      val x1 = x.aggregateByKey(0)((acc, value) => acc + (if (value._13 == "open") 0 else 1), (acc1, acc2) => acc1 + acc2)
       x.join (x1)
   }
 
