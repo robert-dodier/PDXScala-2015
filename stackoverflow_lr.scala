e6741a9a947a2b2d1506dc3482606cf923167caf
Robert Dodier <robert_dodier@users.sourceforge.net>
2015-11-17 12:20:01 -0800

    Fix up Stackoverflow LR example.

--- stackoverflow_lr.scala
+++ stackoverflow_lr.scala
@@ -6,17 +6,20 @@ import org.apache.spark.mllib.classification.LogisticRegressionWithLBFGS
 val lr = new LogisticRegressionWithLBFGS()
 val model = lr.run (x)
 model.getThreshold
-val foo = x.map (p => (model.predict (p.features), p.label))
-val true_positives = foo.filter (ab => ab._1 == 1.0 && ab._2 == 1.0).count
-val false_positives = foo.filter (ab => ab._1 == 1.0 && ab._2 == 0.0).count
-val false_negatives = foo.filter (ab => ab._1 == 0.0 && ab._2 == 1.0).count
-val true_negatives = foo.filter (ab => ab._1 == 0.0 && ab._2 == 0.0).count
+val output_class = x.map (p => (model.predict (p.features), p.label))
+val true_positives = output_class.filter (ab => ab._1 == 1.0 && ab._2 == 1.0).count
+val false_positives = output_class.filter (ab => ab._1 == 1.0 && ab._2 == 0.0).count
+val false_negatives = output_class.filter (ab => ab._1 == 0.0 && ab._2 == 1.0).count
+val true_negatives = output_class.filter (ab => ab._1 == 0.0 && ab._2 == 0.0).count
 model.clearThreshold
-val bar = x.map (p => (p.label, model.predict (p.features)))
+val output_score = x.map (p => (model.predict (p.features), p.label))
 import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
-val metrics = new BinaryClassificationMetrics (foo, 20)
+val metrics = new BinaryClassificationMetrics (output_score, 20)
+val roc = metrics.roc.collect
 metrics.areaUnderROC
 
-val roc = metrics.roc.collect
 import com.quantifind.charts.Highcharts._
 line (roc)
+title ("ROC")
+xAxis ("False Positive rate")
+yAxis ("True Positive rate")
